7a440612ee1e7a4176731288de72ec0f1b513080
unknown <nidhal.nidhal009@gail.com>
2026-05-01 14:09:09 +0200

    redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,14 +1,14 @@
 const { createClient } = require('redis');
 
 const client = createClient({
-  url: 'redis://192.168.75.129:6379'  // Your VM IP
+  url: 'redis://192.168.75.129:6379' 
 });
 
 client.on('error', (err) => console.log('Redis Client Error', err));
 
 async function connectRedis() {
   await client.connect();
-  console.log('Redis connected');
+  console.log('Rediis connected');
 }
 
 module.exports = { client, connectRedis };
